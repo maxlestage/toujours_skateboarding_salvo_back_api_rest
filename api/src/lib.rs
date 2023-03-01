5c0a531d618286b9f86093e362990fec7b13420e
maxlestage <maxlestage@icloud.com>
2023-03-01 19:43:26 +0100

    Config first route

--- api/src/lib.rs
+++ api/src/lib.rs
@@ -2,13 +2,26 @@ use salvo::prelude::*;
 
 #[handler]
 async fn hello_world() -> &'static str {
-    "tottotootootototootoot"
+    "Hello there!"
 }
+
+#[handler]
+
+async fn hello_by_id(req: &mut Request) -> String {
+    req.params().get("id").cloned().unwrap_or_default()
+}
+
 #[tokio::main]
 pub async fn main() {
     database_connection::main().await.expect("Error");
-    let router = Router::new().get(hello_world);
+
+    // Define Routing tree
+    let routing = Router::with_path("")
+        .get(hello_world)
+        .push(Router::with_path("<id>").get(hello_by_id));
+
+    // Server Ready
     Server::new(TcpListener::bind("127.0.0.1:7878"))
-        .serve(router)
+        .serve(routing)
         .await;
 }
