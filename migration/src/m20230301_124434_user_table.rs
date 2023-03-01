482bd63c86520c6154c8d3608886db78a2a07a9f
maxlestage <maxlestage@icloud.com>
2023-03-01 14:20:52 +0100

    Migration Conf and first Push into : OK

---
+++ migration/src/m20230301_124434_user_table.rs
@@ -0,0 +1,54 @@
+use sea_orm_migration::prelude::*;
+
+#[derive(DeriveMigrationName)]
+pub struct Migration;
+
+#[async_trait::async_trait]
+impl MigrationTrait for Migration {
+    async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        // Replace the sample below with your own migration scripts
+
+        manager
+            .create_table(
+                Table::create()
+                    .table(User::Table)
+                    .if_not_exists()
+                    .col(
+                        ColumnDef::new(User::Id)
+                            .integer()
+                            .not_null()
+                            .auto_increment()
+                            .primary_key(),
+                    )
+                    .col(ColumnDef::new(User::Firstname).string().not_null())
+                    .col(ColumnDef::new(User::Lastname).string().not_null())
+                    .col(ColumnDef::new(User::SignUpDate).date().not_null())
+                    .col(ColumnDef::new(User::Mail).string().not_null())
+                    .col(ColumnDef::new(User::Password).string().not_null())
+                    .col(ColumnDef::new(User::Role).string().not_null())
+                    .to_owned(),
+            )
+            .await
+    }
+
+    async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        // Replace the sample below with your own migration scripts
+
+        manager
+            .drop_table(Table::drop().table(User::Table).to_owned())
+            .await
+    }
+}
+
+/// Learn more at https://docs.rs/sea-query#iden
+#[derive(Iden)]
+enum User {
+    Table,
+    Id,
+    Firstname,
+    Lastname,
+    SignUpDate,
+    Mail,
+    Password,
+    Role,
+}
